1564c8e0486a03b66ebba31aa542d01f060b4793
xiang cui <xiangcui@live.com>
2026-05-04 12:27:19 -0400

    updated restapi.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -29,6 +29,7 @@ def get_request(endpoint, **kwargs):
         return response.json()
     except Exception as err:
         # If any error occurs
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
 
@@ -52,8 +53,9 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
